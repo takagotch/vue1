80484e8886e05ab216947127df12526bb5f525b6
takagotch <dyaccb@gmail.com>
2018-01-02 08:57:10 +0900

    tsk/app

---
+++ tsk/app.js
@@ -0,0 +1,12 @@
+new Vue ({
+  el: '#board',
+  data: {
+    tasks: [
+      { name: '', status: 1, assignee: 'A', mandays: 3 }
+      { name: '', status: 1, assignee: 'B', mandays: 2 }
+      { name: '', status: 2, assignee: 'C', mandays: 1 }
+      { name: '', status: 3, assignee: 'D', mandays: 1 }
+    ]
+  }
+})
+
