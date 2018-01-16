718a233cc70b744a62cce44c811f52b05c5cbed9
takagotch <dyaccb@gmail.com>
2018-01-16 20:20:17 +0900

    getxxx(){ getxxx)(.then((battles) => { this.xxx = battles; });},

---
+++ fr3/publicBattles.vue
@@ -0,0 +1,36 @@
+<template>
+</template>
+
+<script>
+import AppNav from './AppNav';
+import { isLoggedIn } from '../../utils/auth';
+import { getPublicStartupBattles } from '../../utils/battles-api';
+export default {
+  name: 'publicBattles',
+  components: {
+    AppNav,
+  },
+  data() {
+    return {
+      publicBattles: '',
+    };
+  },
+  methods: {
+    isLoggedIn() {
+      return isLoggedIn();
+    },
+    getPublicStartupBattles() {
+      getPublicStartupBattles().then((battles) => {
+        this.publicBattles = battles;
+      });
+    },
+  },
+  mounted() {
+    this.getPublicStarupBattles();
+  },
+};
+</script>
+
+<style scoped>
+</style>
+
