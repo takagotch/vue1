1bf3677e30708a0a07051132f387d1c5bfa387c3
takagotch <dyaccb@gmail.com>
2018-01-16 21:57:41 +0900

    data(){ retern{ publicBattles: '', }; },

---
+++ fr3/publicBattle.vue
@@ -0,0 +1,38 @@
+<template>
+
+</template>
+
+<script>
+  import AppNav from './AppNav';
+  import { isLoggedIn } from '../../utils/auth';
+  import { getPublicStartupBattles } from '../../utils/battles-api';
+
+  export default {
+    name: 'publicBattle',
+    components: {
+      AppNav,
+    },
+    data() {
+      return {
+        publicBattles: '',
+    };
+  },
+  maehods: {
+    isLoggedIn() {
+      return isLoggedIn();
+    },
+    getPublicStartupBattles(){
+      getPublicStartupBattles().then((battles) => {
+        this.publicBattles = battles;
+      });
+    },
+  },
+  mounted() {
+    this.getPublicStartupBattles();
+  },
+};
+</script>
+
+<style scioed>
+</style>
+
