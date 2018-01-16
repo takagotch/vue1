46fcc96b92adae75e9f40520dcc6a8dedb8e2f85
takagotch <dyaccb@gmail.com>
2018-01-16 20:13:34 +0900

    class='btn btn-danger log' @click='handleLogout'

--- fr3/AppNav.vue
+++ fr3/AppNav.vue
@@ -1,4 +1,15 @@
 <template>
+  <nav class="navbar navbar-default">
+    <div class="navbar-header">
+      <router-link to="/" class="navbar-brand">The Ultimate... </router>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+        <button class="btn btn-danger log" @click="handleLogout()">Lou out</button>
+	<button class="btn btn-info log" @click="handleLogin()">Log In</button>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
