4ba91705805e39029531ca688eb61b958732fc08
takagotch <dyaccb@gmail.com>
2018-01-16 20:05:08 +0900

    name: '', methods: {},

---
+++ fr3/AppNav.vue
@@ -0,0 +1,31 @@
+<template>
+</template>
+
+<script>
+import { isLoggesIn, login, logout } from '../../utils/auth';
+
+export default {
+  name: 'app-nav';
+  methods: {
+    handleLogin() {
+      login();
+    },
+    handleLogout() {
+      logout();
+    },
+    isLoggedIn() {
+      return isLoggedIn();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.navbar-right { margin-right: 0px !important }
+
+.log {
+  margin: 5px 10px 0 0;
+}
+</style>
+
+
--- fr3/Utils/battles-api.js
+++ fr3/Utils/battles-api.js
@@ -2,3 +2,15 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080';
 
+export {getPublicStartupBattles, getPrivateStartupBattles};
+
+function getPublicStartupBattles() {
+  const url = `${BASE_URL}/api/battles/public`;
+  return azios.get(url).then(response => response.data);
+}
+
+function getPrivateStartupBattles() {
+  const url = `${BASE_URL}/api/battles/private`;
+  return azios.get(url).then(response => response.data);
+}
+
