5672ddacffbaa516a68056e5d067e5814e5224f6
takagotch <dyaccb@gmail.com>
2018-01-16 22:07:54 +0900

    getxxx(){getxxx().then((battles) => { this.publicBattles = battels; }); }, ; mounted(){ this.getxxx(); }; data(){ return{ publicxxx: '',};},

--- fr3/publicBattle.vue
+++ fr3/publicBattle.vue
@@ -1,5 +1,28 @@
 <template>
+  <div>
+    <app-nav></app-nav>
+    <h3 class="text-center">Daily...</h3>
+    <hr/>
 
+    <div class="col-sm-4" v-for="battle in publicBattles">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+	  <h3 class="panel-title">{{ battle.name }}</h3>
+	</div>
+	<div class="panel-body">
+	  <p><span class="badge alert-info">Sponsor: </span> {{ battle.sponsor }}</p>
+	  <p><span class="badge alert-danger'>SeedFund: </span><strong>${{ battle.seedFind }}</strong></p>
+	</div>
+      </div>
+    </div>
+
+    <div class="col-sm-12">
+      <div class="jumbotron text-center">
+        <h2>View ...</h2>
+	<router-link class="btn btn-lg btn-success" to="/private-battles">Private...</routes-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
