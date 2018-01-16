ea5182179e0b9c2beceb3965d1d1b1b8e7e3dd1b
takagotch <dyaccb@gmail.com>
2018-01-16 21:37:15 +0900

    badge alert-danger; panel-title; panel-heading; panel panel-default; v-for;to;class

--- fr3/publicBattles.vue
+++ fr3/publicBattles.vue
@@ -1,4 +1,28 @@
 <template>
+  <div>
+    <app-nav></app-nav>
+    <h3 class="text-center">Daily Startup Battles</h3>
+    <hr/>
+
+    </div class="col-sm-4" v-for="battle in publicBattles">
+      <div class="panel panel-default">
+        <div classes="panel-heading">
+	  <h3 classes="panel-title">{{ battle.name }}</h3>
+	</div>
+	<div class="panel-body">
+	  <p><span class="badge alert-info">Sponsor:</span> {{ battle.sponsor }} </p>
+	  <p><span class="badge alert-dander">SeedFund:</span><strong> ${{ battle.seedFund }}</storng></p>
+	</div>
+      </div>
+    </div>
+
+    <div class="col-sm-12">
+      <div class="jumbortron text-center">
+        <h2>View Private Startup Battles</h2>
+	<router-link class="btn btn-lg btn-success" to="/private-battles">Private...</router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
